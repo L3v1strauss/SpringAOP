25f676dfa2d4c4d403d499b5aa92e32081d418af
L3v1strauss <L3v1s@mail.ru>
2021-02-08 20:01:37 +0300

    Spring AOP + announcement PointCut

--- spring-course-aop/src/main/java/com/springcourses/aop/Book.java
+++ spring-course-aop/src/main/java/com/springcourses/aop/Book.java
@@ -1,4 +1,4 @@
-package com.springcourses.aop.aspects;
+package com.springcourses.aop;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
--- spring-course-aop/src/main/java/com/springcourses/aop/UniLibrary.java
+++ spring-course-aop/src/main/java/com/springcourses/aop/UniLibrary.java
@@ -1,16 +1,15 @@
 package com.springcourses.aop;
 
-import com.springcourses.aop.aspects.Book;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UniLibrary extends AbstractLibrary {
 
-    public void getBook(Book book) {
-        System.out.println("Берем книгу из UniLibrary" + " " + book.getName());
+    public void getBook() {
+        System.out.println("Берем книгу из UniLibrary");
     }
 
-    public void getMagazine(int i) {
+    public void getMagazine() {
         System.out.println("Берем журнал из UniLibrary");
     }
 
